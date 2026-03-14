e7f8ab5fe7e1ab91fc9fd5942505cf99777fe05a
japjot singh <japjotsingh461@gmail.com>
2026-03-14 10:42:04 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app=express();
 
+const{ adminAuth, userAuth}=require("./middlewares/auth");
 
-app.use("/user",(req,res,next)=>{
-    console.log("handling the route user 1");
-    next();
-},
-(req,res,next)=>{
-    console.log("handling the route server 2")
- //   res.send("2nd response");
- next();
-},
-(req,res)=>{
-    console.log("handling the route server 2")
-    res.send("2nd response");
-},
-)
+app.use("/admin",adminAuth);
+app.use("/user",userAuth);
+
+app.get("/user",(req,res)=>{
+    res.send("user data sent");
+})
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("all data send");
+});
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("deleted a user");
+});
 
 app.listen(3000,()=>{
     console.log("server is started")
